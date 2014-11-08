0ac34136e92a15bf6f6bae2a6139d97a55c4abd6
Stella Tigre <stella@scrapinghub.com>
2014-11-07 21:23:28 -0600

    logic isolated into small methods, xml templates have their own file

--- usps_labels.py
+++ usps_labels.py
@@ -1,165 +1,26 @@
+import base64
 import requests as req
-
-api_addr = "https://secure.shippingapis.com/ShippingAPI.dll?API=SignatureConfirmationCertifyV4&XML="
-api_user = "564ORDOR2223"
+import xmltodict as xml2dict
+import xml_templates as xmlt    # these are both local modules 
+import configfile as config     # in the same directory
 
 
 # package_info must contain the specific fields we want
 def make_request_xml(base_xml_string, package_info):
     return base_xml_string.format(**package_info)
 
-test_opts = {
-    'api_user' : api_user,
-    'from_address1' : '2000 E 9th street',
-    'from_address2' : 'apt #b',
-    'from_city' : 'Austin',
-    'from_state' : 'Texas',
-    'from_zip5' : 78702
-}
+def get_label(package_info):
+    label_response = req.get(config.api_address + package_info)
+    label_dict = xml2dict.parse(label_response.content)
+    label_image_64 = label_dict['SigConfirmCertifyV4.0Response']['SignatureConfirmationLabel']
+    return (label_image_64, label_dict)
 
-test_xml = '''
-<?xml version="1.0" encoding="UTF-8" ?>
+def save_pdf_from_base64(base64_pdf, image_filename):
+    with open("./pdfs/" + image_filename, "wb") as image_pdf:
+        image_pdf.write(base64.b64decode(base64_pdf))
+        image_pdf.close()
+    return "./pdfs/label_example.pdf"
 
-<SigConfirmCertifyV4.0Request USERID="564ORDOR2223" PASSWORD="XXXXXX">
-
-  <Revision>2</Revision>
-
-  <ImageParameters />
-
-  <FromName>John Doe</FromName>
-
-  <FromFirm>USPS</FromFirm>
-
-  <FromAddress1>RM 2100</FromAddress1>
-
-  <FromAddress2>475 Enfant Plaza SW</FromAddress2>
-
-  <FromCity>Washington</FromCity>
-
-  <FromState>DC</FromState>
-
-  <FromZip5>20260</FromZip5>
-
-  <FromZip4/>
-
-  <ToName>Janice Dickens</ToName>
-
-  <ToFirm>XYZ Corporation</ToFirm>
-
-  <ToAddress1>Ste 100</ToAddress1>
-
-  <ToAddress2>2 Massachusetts Ave NE</ToAddress2>
-
-  <ToCity>Washington</ToCity>
-
-  <ToState>DC</ToState>
-
-  <ToZip5>20212</ToZip5>
-
-  <ToZip4 />
-
-  <ToPOBoxFlag></ToPOBoxFlag>
-
-  <WeightInOunces>10</WeightInOunces>
-
-  <ServiceType>Priority</ServiceType>
-
-  <SeparateReceiptPage>False</SeparateReceiptPage>
-
-  <POZipCode>20770</POZipCode>
-
-  <ImageType>TIF</ImageType>
-
-  <AddressServiceRequested>False</AddressServiceRequested>
-
-  <HoldForManifest>N</HoldForManifest>
-
-  <Container>NONRECTANGULAR</Container>
-
-  <Size>LARGE</Size>
-
-  <Width>7</Width>
-
-  <Length>20.5</Length>
-
-  <Height>15</Height>
-
-  <Girth>60</Girth>
-
-  <ReturnCommitments>true</ReturnCommitments>
-
- </SigConfirmCertifyV4.0Request>
-'''
-
-
-base_request = '''
-    <?xml version="1.0" encoding="UTF-8" ?>
-      <ExpressMailLabelCertifyRequest  USERID="{api_user}">
-        <Option />
-        <Revision>2</Revision>
-        <EMCAAccount />
-        <EMCAPassword />
-        <ImageParameters />
-        <FromFirstName>Adam</FromFirstName>
-        <FromLastName>Smith</FromLastName>
-        <FromFirm/>
-    <FromAddress1>{from_address1}</FromAddress1>
-    <FromAddress2>{from_address2}</FromAddress2>
-    <FromCity>{from_city}</FromCity>
-    <FromState>{from_state}</FromState>
-    <FromZip5>{from_zip5}</FromZip5>
-    <FromZip4/>
-        <FromPhone>2125551234</FromPhone>
-    <ToFirstName>Janice</ToFirstName>
-    <ToLastName>Dickens</ToLastName>
-    <ToFirm>XYZ Corporation</ToFirm>
-    <ToAddress1>Ste 100</ToAddress1>
-    <ToAddress2>2 Massachusetts Ave NE</ToAddress2>
-    <ToCity>Washington</ToCity>
-    <ToState>DC</ToState>
-    <ToZip5>20212</ToZip5>
-    <ToZip4 />
-        <ToPhone>2125551234</ToPhone>
-        <WeightInOunces>105</WeightInOunces>
-        <FlatRate/>
-        <SundayHolidayDelivery/>
-        <StandardizeAddress/>
-        <WaiverOfSignature/>
-        <NoHoliday/>
-        <NoWeekend/>
-        <SeparateReceiptPage/>
-        <POZipCode>20212</POZipCode>
-        <FacilityType>DDU</FacilityType>
-        <ImageType>PDF</ImageType>
-        <LabelDate>10/19/2010</LabelDate>
-        <CustomerRefNo/>
-        <SenderName>Adam Smith</SenderName>
-        <SenderEMail>asmith@email.com</SenderEMail>
-        <RecipientName>Janice Dickens</RecipientName>
-        <RecipientEMail>jdickens@email.com</RecipientEMail>
-        <HoldForManifest/>
-        <CommercialPrice>false</CommercialPrice>
-        <InsuredAmount>425.00</InsuredAmount>
-        <Container>NONRECTANGULAR</Container>
-        <Size>LARGE</Size>
-        <Width>7</Width>
-        <Length>20.5</Length>
-        <Height>15</Height>
-        <Girth>60</Girth>
-      </ExpressMailLabelCertifyRequest>
-'''
-
-
-
-print("\nResponse Content with formatted XML string number 2:")
-rate_response1 = req.get(api_addr + test_xml)
-print(rate_response1.content)
-
-xml_dict = xml.fromstring(rate_response1.content)
-print(xml_dict)
-
-#print("\nResponse Content with formatted XML string number 2:")
-#rate_response = req.post(api_addr, xml_formatted)
-#print(rate_response.content)
-
-
+label_image, response_dict = get_label(xmlt.test_label)
+save_pdf_from_base64(label_image, "new_example.pdf") 
+print("Response as a dict:\n" + str(response_dict))
