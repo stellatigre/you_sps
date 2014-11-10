f8643d2b0d268c89e90524875bd419a3935a0a55
Stella Tigre <stella@scrapinghub.com>
2014-11-10 14:58:17 -0600

    make debug mode part of config file

--- configfile.py
+++ configfile.py
@@ -1,10 +1,11 @@
 # API request options
-#host = "swag.moe" 
+host = "swag.moe:9400" 
 label_api_address = "https://secure.shippingapis.com/ShippingAPI.dll?API=SignatureConfirmationCertifyV4&XML="
 rate_api_address = "http://production.shippingapis.com/ShippingAPI.dll?API=RateV4"
 api_user = "564ORDOR2223"
 image_type = "PDF"
 listen_port = 9400
+debug = True
 
 db = {
     "user" : "usps_api",
